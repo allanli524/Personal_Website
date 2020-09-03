6319fb82d7ca5f9c3c58be57eb56099be81d94c5
allanli524 <allanli524@gmail.com>
2020-09-03 15:21:04 -0600

    template complete

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDom from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import home from "./component/home.js";
+import about from "./component/about.js";
+import experience from "./component/experience.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About me</Link>
+          </li>
+          <li>
+            <Link to="/experience">Experience</Link>
+          </li>
+        </ul>
+      </div>
+      <Switch>
+        <Route exact path="/" component={home}/>
+        <Route exact path="/experience" component={experience}/>
+        <Route exact path="/about" component={about}/>
+        <Route/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ website/src/component/experience.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const experience = () => {
+    return (
+        <div>
+            <h1>Experiences</h1>
+        </div>
+    )
+}
+
+export default experience;
